406fc9e1c28c9cc9c38c12a0eab8df9d2a95a4e1
zpf <363922975@qq.com>
2022-11-24 17:47:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -52,5 +52,5 @@ wea, humidity, temperature = get_weather()
 data = {"weather":{"value":wea},"humidity":{"value":humidity},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
-res2 = wm.send_template(user_id2, template_id, data)
-print(res2)
+res = wm.send_template(user_id2, template_id, data)
+print(res)
