35851502399777afe1d9d87d991498c9be36cbc6
zpf <363922975@qq.com>
2022-11-24 17:31:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,5 +51,5 @@ wm = WeChatMessage(client)
 wea, humidity, temperature = get_weather()
 data = {"weather":{"value":wea},"humidity":{"value":humidity},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
-res = wm.send_template(user_id2, template_id, data)
+res2 = wm.send_template(user_id2, template_id, data)
 print(res)
